9500f31a200cf769c61478c679e18e6dc6196d3f
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2025-11-01 13:31:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // or your frontend URL if you want to restrict it
+    methods: ["GET", "POST"]
+  }
+});
+
 const PORT = process.env.PORT || 3000
 app.use(express.static("public"));
 
